e029e832511eb8f02e1ae581f8824f141508a966
rariig <raoul.rocco@gmail.com>
2022-11-23 18:57:56 +0200

    Started creating functions to parse customs that have often recurring tasks (more often than each day).

--- Cron_parse/parse.js
+++ Cron_parse/parse.js
@@ -21,17 +21,36 @@ let taskDates  = class{
 
 // consists of 3 parts: 'cron_startDate endDate_daysToExecute'
 var custom = 
-//'0 0 * 11 *_4 22 5 33_21'; //every day during nov 4.22-5.33 
-//'0 0 12,26,28 * *_4 22 5 33_21'; //every 12,26,28 day during any month at 4.22-5.33 for 21 days from start
-'0 0 * * *_4 22 5 33_21'; // every day for 21 days at 4.22-5.33
-//'0 0 * * MON,WED_4 22 5 33_21'; // every Monday and Wednesday for 21 days at 4.22-5.33
-//'0 */3 * * *_4 22 5 33_21'; // every 3 hours starting from 4.22 today for 21 days
+//'* * * 11 *_4 22 5 33_21'; //every day during nov 4.22-5.33 
+//'* * 12,26,28 * *_4 22 5 33_21'; //every 12,26,28 day during any month at 4.22-5.33 for 21 days from start
+//'* * * * *_4 22 5 33_21'; // every day for 21 days at 4.22-5.33
+//'* * * * MON,WED_4 22 5 33_21'; // every Monday and Wednesday for 21 days at 4.22-5.33
+'*/30 */3 * * *_4 22 5 33_21'; // every 3 hours starting from 4.22 today for 21 days
 
 var cronExprArray = [];
 var tasksArray = [];
 var cronsWithLengthsArray = [];
 var allTaskDatesArray = [];
+var arrayOfCronForOftenRecurringTask=[];
 var finalArray= [];
+
+function getRecurrenceInMinutes(customExpression){
+    var recurrenceInMinutes =0;
+    if (customExpression.split(' ')[0].indexOf('/')> -1) {
+      recurrenceInMinutes=parseInt(recurrenceInMinutes +  customExpression.split(' ')[0].split('/')[1]);
+    }
+    if (customExpression.split(' ')[1].indexOf('/')> -1) {
+        recurrenceInMinutes= parseInt(recurrenceInMinutes +  60*customExpression.split(' ')[1].split('/')[1]);
+    }
+    return recurrenceInMinutes;
+}
+
+//based on custom expression. start and end time will change.(tasklength stays) days to run stays.
+function generateRecurringCrons(customExpression){ 
+   var hoursToAdd= parseInt(getRecurrenceInMinutes(customExpression)/60)
+   minutesToAdd = getRecurrenceInMinutes(customExpression) % 60
+    return minutesToAdd;
+}
 
 function getTaskLength (customExpression){
     var customTaskLength = customExpression.split('_')[1];    
@@ -46,12 +65,20 @@ function getDaysToRun(customExpression){
     return customDaysToRun;  
 }
 
+function replaceCronMinutesAndHours(customExpression){
+    var cron = customExpression.split('_')[0];
+    var customTaskLength = customExpression.split('_')[1];
+    cron = cron.replace(cron.split(' ')[0], customTaskLength.split(' ')[1])
+    cron = cron.replace(cron.split(' ')[1], customTaskLength.split(' ')[0])
+    return cron;
+}
+
 function getCron(customExpression){
     var cron = customExpression.split('_')[0];
     var customTaskLength = customExpression.split('_')[1];
     cron = cron.replace(cron.split(' ')[0], customTaskLength.split(' ')[1])
     cron = cron.replace(cron.split(' ')[1], customTaskLength.split(' ')[0])
-    cronExprArray.push(cron)
+    //cronExprArray.push(cron)
     return cron;
 }
 
@@ -96,7 +123,7 @@ function findEndDates(cronsWithLengthsArray, numberOfDays){
     }
 }
 
-try {
+/*try {
     getCron(custom);
     getTaskLength(custom);
     mergeCronTasks(cronExprArray, tasksArray , getDaysToRun(custom));
@@ -106,17 +133,16 @@ try {
 }
 catch (err) {
     console.log('Error: ' + err.message);
-}
+}*/
 
 
-/*try {
-    console.log(getTaskLength(custom));
-    console.log(getDaysToRun(custom));
-    console.log(getCron(custom));
+try {
+    console.log(getRecurrenceInMinutes(custom));
+    console.log(generateRecurringCrons(custom));
 
 }
 catch (err) {
     console.log('Error: ' + err.message);
-}*/
+}
 
     
